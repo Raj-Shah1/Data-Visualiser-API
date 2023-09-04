fedf18e26c029313908d7fcebf141bbca70b7797
Raj Shah <rajshah0211@gmail.com>
2023-09-04 18:42:53 +0530

    Route changes

--- app/routes.py
+++ app/routes.py
@@ -7,8 +7,8 @@ from app.helper import format_column_headers
 from app.service.fetch_graph_data import table_data
 
 
-@app.route("/ask_openai", methods=["POST"])
-def ask_openai_route():
+@app.route("/generate-query", methods=["POST"])
+def generate_query_route():
     user_question = request.form.get("user_question")
     if user_question:
         answer = openai_query_generation(user_question)
@@ -17,13 +17,13 @@ def ask_openai_route():
         return "Please provide a user question in the 'user_question' parameter.", 400
 
 
-@app.route("/execute_query", methods=["POST"])
+@app.route("/execute-query", methods=["POST"])
 def execute_query_route():
     user_query = request.form.get("user_query")
 
     google_table_data = table_data(user_query)
 
     if google_table_data:
-        return Response(str(google_table_data), mimetype="text/plain")
+        return Response(google_table_data, mimetype="text/plain")
     else:
         return "Please provide a user query in the 'user_query' parameter.", 400
