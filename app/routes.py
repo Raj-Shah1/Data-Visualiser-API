92d1ca0c7edf686e04a1704f032c4668ecdf9640
Raj Shah <rajshah0211@gmail.com>
2023-09-05 12:14:08 +0530

    Renamed variables and response of routes

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,8 @@
-from decimal import Decimal
 from flask import Response, request, jsonify
 from app import app
 from app.openai_api import openai_query_generation
-from app.db import execute_query, create_sql_queries_table, create_sql_queries_record, get_sql_queries_by_name
-from app.helper import format_column_headers
 from app.service.fetch_graph_data import table_data
+import json
 
 
 @app.route("/generate-query", methods=["POST"])
@@ -14,21 +12,28 @@ def generate_query_route():
     if json_data is None:
         return jsonify({"error": "Invalid JSON data"}), 400
 
-    user_question = str(json_data['user_question'])
-    if user_question:
-        answer = openai_query_generation(user_question)
+    question = str(json_data['question'])
+    if question:
+        answer = openai_query_generation(question)
         return Response(answer, mimetype="text/plain")
     else:
-        return "Please provide a user question in the 'user_question' parameter.", 400  
+        return "Please provide a user question in the 'question' parameter.", 400  
 
 
 @app.route("/execute-query", methods=["POST"])
 def execute_query_route():
-    user_query = request.form.get("user_query")
+    
+    json_data = request.json
+    if json_data is None:
+        return jsonify({"error": "Invalid JSON data"}), 400
+    
+    query = str(json_data['query'])
 
-    google_table_data = table_data(user_query)
+    google_table_data = table_data(query)
 
     if google_table_data:
-        return Response(google_table_data, mimetype="text/plain")
+        # Convert the data to JSON and encode it as bytes
+        json_data_bytes = json.dumps(google_table_data).encode('utf-8')
+        return Response(json_data_bytes, mimetype="application/json")
     else:
         return "Please provide a user query in the 'user_query' parameter.", 400
